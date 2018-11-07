4e9284543821227c5dba5d404e4e263289a9373a
Mateusz Bednarski <mateusz.bednarski@roche.com>
2018-11-07 22:56:47 +0100

    Initial version of dataset.py

--- dataset.py
+++ dataset.py
@@ -6,28 +6,22 @@ class Dataset:
         self.test_dir = Path('data/raw/aclImdb/test')
 
     def _get_set(self, limit, directory):
-        if limit is None:
-            assert False
-        else:
-            x = []
-            y = []
-            counter = 0
-            for file in (directory / 'pos').iterdir():
-                counter += 1
-                if counter >= limit / 2:
-                    break
-                x.append(file.read_text())
-                y.append(1)
-            for file in (directory / 'neg').iterdir():
-                counter += 1
-                if counter >= limit:
-                    break
-                x.append(file.read_text())
-                y.append(0)
+        x = []
+        y = []
+        for file in (directory / 'pos').iterdir():
+            if limit is not None and len(x) >= limit / 2:
+                break
+            x.append(file.read_text())
+            y.append(1)
+        for file in (directory / 'neg').iterdir():
+            if limit is not None and len(x) >= limit:
+                break
+            x.append(file.read_text())
+            y.append(0)
         return x, y
 
-    def get_train_set(self, limit=500):
+    def get_train_set(self, limit):
         return self._get_set(limit=limit, directory=self.train_dir)
 
-    def get_test_set(self, limit=500):
+    def get_test_set(self, limit):
         return self._get_set(limit=limit, directory=self.test_dir)
