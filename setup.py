18a62f4a2127f47276ea7ab6927bf55a11705b4a
Mateusz Bednarski <mateusz.bednarski@windowslive.com>
2018-11-08 20:32:15 +0100

    Console entry point

--- setup.py
+++ setup.py
@@ -3,4 +3,8 @@ from setuptools import setup
 setup(name='mlp',
       packages=['mlp'],
       version='0.0.1dev1',
-      )
+      entry_points={
+            'console_scripts': ['mlp-cli=mlp.cmd:main']
+      }
+)
+    
