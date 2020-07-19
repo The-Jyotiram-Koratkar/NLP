0d968f61427c0f9dda9a543475cf468ca1e8cefb
The-Jyotiram-Koratkar <61285024+The-Jyotiram-Koratkar@users.noreply.github.com>
2020-07-19 14:20:27 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,11 @@ setup(name='mlp',
       version='0.0.1dev1',
       entry_points={
             'console_scripts': ['mlp-cli=mlp.cmd:main']
+      author = 'Jyotiram Koratkar',
+      author_email = 'jyotiramsakshi10@gmail.com',
+      url = 'https://github.com/The-Jyotiram-Koratkar/NLP',
+      download_url = 'https://github.com/The-Jyotiram-Koratkar/NLP/archive/V1.tar.gz',
+    keywords = ['tag1', 'tag2'],',
+      keywords = ['tag1', 'tag2'],
       }
 )
-    
